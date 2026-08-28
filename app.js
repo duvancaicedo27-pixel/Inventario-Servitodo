f63dc48fede7cb14f786a25bfae9704c129b9cf9
duvancaicedo27-pixel <duvancaicedo27@gmail.com>
2026-08-28 10:52:40 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1858,7 +1858,7 @@ async function sincronizarXCCPendientes(){
         programarReintentoSincronizacion();
     }
 }
-function generarReporteXCC(){
+async function generarReporteXCC(){
     const momento=new Date();
     const fechaReporte=momento.toLocaleDateString("es-CO",{day:"2-digit",month:"2-digit",year:"numeric"});
     const horaReporte=momento.toLocaleTimeString("es-CO",{hour:"2-digit",minute:"2-digit",second:"2-digit",hour12:false});
